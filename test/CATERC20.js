c6fe91b36eeb3df7344a6fad99c53f508e1400c3
Sheikh Wasif <wasif28@hotmail.com>
2023-04-18 15:42:20 +0500

    cross chain test cases for ERC20 (#21)

--- test/CATERC20.js
+++ test/CATERC20.js
@@ -1,6 +1,11 @@
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { ethers, upgrades } = require("hardhat");
 const { expect } = require("chai");
+const elliptic = require("elliptic");
+const { ZERO_BYTES32, ZERO_ADDRESS } = require("@openzeppelin/test-helpers/src/constants");
+const { web3 } = require("@openzeppelin/test-helpers/src/setup");
+
+const testSigner1PK = "cfb12303a19cde580bb4dd771639b0d26bc68353645571a8cff516ab2ee113a0";
 
 const name = "CATERC20Test";
 const symbol = "CATTEST";
@@ -8,7 +13,7 @@ const decimals = 18;
 const maxSupply = "1000000000000000000000";
 
 const wormholeChainId = "1";
-const wormholeCoreContract = "0x9BF4eb2fd0E414B813E5253811055238E8923A48";
+const wormholeCoreContract = "0x98f3c9e6E3fAce36bAAd05FE09d375Ef1464288B";
 const finality = 1;
 const nowTime = parseInt(Math.floor(new Date().getTime() / 1000));
 const validTime = nowTime + 300;
@@ -157,12 +162,118 @@ describe("CATERC20", () => {
   describe("Minting New Tokens", () => {
     it("Should Mint new tokens only by owner", async () => {
       const { owner, otherAccount, TestTokenInstance, CATERC20Instance } = await deployFixture();
-      const amountToMint = "1000000000"
+      const amountToMint = "1000000000";
 
       await CATERC20Instance.connect(owner).mint(owner.address, amountToMint);
-      expect (await CATERC20Instance.connect(owner).balanceOf(owner.address)).to.equal(amountToMint);
+      expect(await CATERC20Instance.connect(owner).balanceOf(owner.address)).to.equal(amountToMint);
 
-      await expect(CATERC20Instance.connect(otherAccount).mint(otherAccount.address,amountToMint)).to.be.reverted
+      await expect(CATERC20Instance.connect(otherAccount).mint(otherAccount.address, amountToMint))
+        .to.be.reverted;
+    });
+  });
+
+  describe("Cross Chain Transfers", () => {
+    it("bridgeOut", async () => {
+      const { owner, otherAccount, TestTokenInstance, CATERC20Instance } = await deployFixture();
+
+      const amountToMint = "100000000000000000000";
+
+      await CATERC20Instance.mint(owner.address, amountToMint);
+      await CATERC20Instance.bridgeOut(
+        amountToMint,
+        wormholeChainId,
+        await CATERC20Instance.addressToBytes(owner.address),
+        0
+      );
+    });
+
+    it("bridgeIn", async () => {
+      const { owner, otherAccount, TestTokenInstance, CATERC20Instance } = await deployFixture();
+
+      const amountToMint = "100000000000000000000";
+
+      const data = {
+        amount: amountToMint,
+        tokenAddress: await CATERC20Instance.addressToBytes(CATERC20Instance.address),
+        tokenChain: 2,
+        toAddress: await CATERC20Instance.addressToBytes(owner.address),
+        toChain: 2,
+      };
+
+      const payload = await CATERC20Instance.encodeTransfer(data);
+      const vaa = await signAndEncodeVM(
+        1,
+        1,
+        wormholeChainId,
+        await CATERC20Instance.addressToBytes(CATERC20Instance.address),
+        0,
+        payload,
+        [testSigner1PK],
+        0,
+        finality
+      );
+      console.log("VAA: ", vaa);
+
+      await CATERC20Instance.bridgeIn("0x" + vaa);
     });
   });
 });
+
+const signAndEncodeVM = async function (
+  timestamp,
+  nonce,
+  emitterChainId,
+  emitterAddress,
+  sequence,
+  data,
+  signers,
+  guardianSetIndex,
+  consistencyLevel
+) {
+  const body = [
+    web3.eth.abi.encodeParameter("uint32", timestamp).substring(2 + (64 - 8)),
+    web3.eth.abi.encodeParameter("uint32", nonce).substring(2 + (64 - 8)),
+    web3.eth.abi.encodeParameter("uint16", emitterChainId).substring(2 + (64 - 4)),
+    web3.eth.abi.encodeParameter("bytes32", emitterAddress).substring(2),
+    web3.eth.abi.encodeParameter("uint64", sequence).substring(2 + (64 - 16)),
+    web3.eth.abi.encodeParameter("uint8", consistencyLevel).substring(2 + (64 - 2)),
+    data.substr(2),
+  ];
+
+  const hash = web3.utils.soliditySha3(web3.utils.soliditySha3("0x" + body.join("")));
+
+  let signatures = "";
+
+  for (let i in signers) {
+    const ec = new elliptic.ec("secp256k1");
+    const key = ec.keyFromPrivate(signers[i]);
+    const signature = key.sign(hash.substr(2), { canonical: true });
+
+    const packSig = [
+      web3.eth.abi.encodeParameter("uint8", i).substring(2 + (64 - 2)),
+      zeroPadBytes(signature.r.toString(16), 32),
+      zeroPadBytes(signature.s.toString(16), 32),
+      web3.eth.abi.encodeParameter("uint8", signature.recoveryParam).substr(2 + (64 - 2)),
+    ];
+
+    signatures += packSig.join("");
+  }
+
+  const vm = [
+    web3.eth.abi.encodeParameter("uint8", 1).substring(2 + (64 - 2)),
+    web3.eth.abi.encodeParameter("uint32", guardianSetIndex).substring(2 + (64 - 8)),
+    web3.eth.abi.encodeParameter("uint8", signers.length).substring(2 + (64 - 2)),
+
+    signatures,
+    body.join(""),
+  ].join("");
+
+  return vm;
+};
+
+function zeroPadBytes(value, length) {
+  while (value.length < 2 * length) {
+    value = "0" + value;
+  }
+  return value;
+}
--- test/CATERC20ParentChain.js
+++ test/CATERC20ParentChain.js
@@ -1,9 +1,14 @@
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { ethers, upgrades } = require("hardhat");
 const { expect } = require("chai");
+const elliptic = require("elliptic");
+const { ZERO_BYTES32, ZERO_ADDRESS } = require("@openzeppelin/test-helpers/src/constants");
+const { web3 } = require("@openzeppelin/test-helpers/src/setup");
+
+const testSigner1PK = "cfb12303a19cde580bb4dd771639b0d26bc68353645571a8cff516ab2ee113a0";
 
 const wormholeChainId = "1";
-const wormholeCoreContract = "0x9BF4eb2fd0E414B813E5253811055238E8923A48";
+const wormholeCoreContract = "0x98f3c9e6E3fAce36bAAd05FE09d375Ef1464288B";
 const finality = 1;
 const nowTime = parseInt(Math.floor(new Date().getTime() / 1000));
 const validTime = nowTime + 300;
@@ -146,12 +151,121 @@ describe("CATERC20ParentChain", () => {
       const encoded = await CATERC20ParentChainInstance.encodeTransfer(data);
       const decoded = await CATERC20ParentChainInstance.decodeTransfer(encoded);
 
-      expect(decoded.amount).to.equal(data.amount)
-      expect(decoded.tokenAddress).to.equal(data.tokenAddress)
-      expect(decoded.tokenChain).to.equal(data.tokenChain)
-      expect(decoded.toAddress).to.equal(data.toAddress)
-      expect(decoded.toChain).to.equal(data.toChain)
+      expect(decoded.amount).to.equal(data.amount);
+      expect(decoded.tokenAddress).to.equal(data.tokenAddress);
+      expect(decoded.tokenChain).to.equal(data.tokenChain);
+      expect(decoded.toAddress).to.equal(data.toAddress);
+      expect(decoded.toChain).to.equal(data.toChain);
+    });
+  });
 
+  describe("Cross Chain Transfers", () => {
+    it("bridgeOut", async () => {
+      const { owner, otherAccount, TestTokenInstance, CATERC20ParentChainInstance } =
+        await deployFixture();
+
+      const amountToMint = "100000000000000000000";
+
+      await TestTokenInstance.mint(owner.address, amountToMint);
+      await TestTokenInstance.approve(CATERC20ParentChainInstance.address, amountToMint);
+      await CATERC20ParentChainInstance.bridgeOut(
+        amountToMint,
+        wormholeChainId,
+        await CATERC20ParentChainInstance.addressToBytes(owner.address),
+        0
+      );
+    });
+
+    it("bridgeIn", async () => {
+      const { owner, otherAccount, TestTokenInstance, CATERC20ParentChainInstance } =
+        await deployFixture();
+
+      const amountToMint = "100000000000000000000";
+
+      const data = {
+        amount: amountToMint,
+        tokenAddress: await CATERC20ParentChainInstance.addressToBytes(
+          CATERC20ParentChainInstance.address
+        ),
+        tokenChain: 2,
+        toAddress: await CATERC20ParentChainInstance.addressToBytes(owner.address),
+        toChain: 2,
+      };
+
+      const payload = await CATERC20ParentChainInstance.encodeTransfer(data);
+      const vaa = await signAndEncodeVM(
+        1,
+        1,
+        wormholeChainId,
+        await CATERC20ParentChainInstance.addressToBytes(CATERC20ParentChainInstance.address),
+        0,
+        payload,
+        [testSigner1PK],
+        0,
+        finality
+      );
+      console.log("VAA: ", vaa);
+
+      await CATERC20ParentChainInstance.bridgeIn("0x" + vaa);
     });
   });
 });
+
+const signAndEncodeVM = async function (
+  timestamp,
+  nonce,
+  emitterChainId,
+  emitterAddress,
+  sequence,
+  data,
+  signers,
+  guardianSetIndex,
+  consistencyLevel
+) {
+  const body = [
+    web3.eth.abi.encodeParameter("uint32", timestamp).substring(2 + (64 - 8)),
+    web3.eth.abi.encodeParameter("uint32", nonce).substring(2 + (64 - 8)),
+    web3.eth.abi.encodeParameter("uint16", emitterChainId).substring(2 + (64 - 4)),
+    web3.eth.abi.encodeParameter("bytes32", emitterAddress).substring(2),
+    web3.eth.abi.encodeParameter("uint64", sequence).substring(2 + (64 - 16)),
+    web3.eth.abi.encodeParameter("uint8", consistencyLevel).substring(2 + (64 - 2)),
+    data.substr(2),
+  ];
+
+  const hash = web3.utils.soliditySha3(web3.utils.soliditySha3("0x" + body.join("")));
+
+  let signatures = "";
+
+  for (let i in signers) {
+    const ec = new elliptic.ec("secp256k1");
+    const key = ec.keyFromPrivate(signers[i]);
+    const signature = key.sign(hash.substr(2), { canonical: true });
+
+    const packSig = [
+      web3.eth.abi.encodeParameter("uint8", i).substring(2 + (64 - 2)),
+      zeroPadBytes(signature.r.toString(16), 32),
+      zeroPadBytes(signature.s.toString(16), 32),
+      web3.eth.abi.encodeParameter("uint8", signature.recoveryParam).substr(2 + (64 - 2)),
+    ];
+
+    signatures += packSig.join("");
+  }
+
+  const vm = [
+    web3.eth.abi.encodeParameter("uint8", 1).substring(2 + (64 - 2)),
+    web3.eth.abi.encodeParameter("uint32", guardianSetIndex).substring(2 + (64 - 8)),
+    web3.eth.abi.encodeParameter("uint8", signers.length).substring(2 + (64 - 2)),
+
+    signatures,
+    body.join(""),
+  ].join("");
+
+  return vm;
+};
+
+function zeroPadBytes(value, length) {
+  while (value.length < 2 * length) {
+    value = "0" + value;
+  }
+  return value;
+}
